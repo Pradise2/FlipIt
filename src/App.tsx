dce523a64151302a392f34f9e48c3688e492b36d
Pradise2 <akinsunmitaye@gmail.com>
2025-02-21 21:00:33 +0100

    Add newfile.txt

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Pvc from "./components/PvcSection/Pvc";
+
 import "./App.css";
+
+
+
 
 export function App() {
 
   return (
-   <Router>
+    
+          <Router>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/pvc/*" element={<Pvc />} />
             </Routes>
           </Router>
+        
+    
   );
 }
 
