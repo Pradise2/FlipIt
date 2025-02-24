4813ccf8a89520da83444c4dcaa2d1f78f5f9fe3
Pradise2 <akinsunmitaye@gmail.com>
2025-02-24 16:27:52 +0100

    Add newfile.txt

--- src/components/PvcSection/components/CreateGameVsComp.tsx
+++ src/components/PvcSection/components/CreateGameVsComp.tsx
@@ -45,29 +45,31 @@ const FlipCoin = () => {
     won: boolean | null;
     result: string | null;
   }>({ won: null, result: null });
-
+  
   async function setupContract() {
     try {
       if (!isConnected) {
         throw new Error("User is disconnected.");
-      } else {
-        // Code to execute if the user is connected
-        throw new Error("User is connected. Proceeding with contract setup...");
-        // You can add further logic here, such as setting up a contract, etc.
       }
       
-  
-    const ethersProvider = new BrowserProvider(walletProvider as unknown as ethers.Eip1193Provider);
-    const signer = await ethersProvider.getSigner();
-    const userAddress = await signer.getAddress();
-    console.log("Address:", userAddress);
-    const contract = new ethers.Contract(ADDRESS, ABI, signer);
-    return { signer, contract };
-  } catch (error) {
-    console.error("Error setting up contract:", error);
-    throw error;  // Rethrow or handle as needed
+      // Code to execute if the user is connected
+      if (!walletProvider) {
+        throw new Error("Wallet provider is not available");
+      }
+      
+      const ethersProvider = new BrowserProvider(walletProvider as unknown as ethers.Eip1193Provider);
+      const signer = await ethersProvider.getSigner();
+      const userAddress = await signer.getAddress();
+      console.log("Address:", userAddress);
+      
+      const contract = new ethers.Contract(ADDRESS, ABI, signer);
+      return { signer, contract };
+    } catch (error) {
+      console.error("Error setting up contract:", error);
+      throw error;  // Rethrow or handle as needed
+    }
   }
-}
+  
   const flip = async (
     tokenAddress: string,
     tokenAmount: string,
